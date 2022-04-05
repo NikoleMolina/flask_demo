cd46913d42f9d5b27d790f3df5bd054a323dff15
Nikole Molina <nikole-molina@uiowa.edu>
2022-04-04 22:11:27 -0500

    new pages

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request, url_for
 
 app = Flask(__name__)
+
+friend_list = [{"name": "Mike Colbert"} ]
 
 @app.route('/')
 def index():
@@ -10,6 +12,19 @@ def index():
 @app.route('/mike')
 def mike():
     return render_template('mike.html', pageTitle='About Mike')
+
+@app.route('/add_friend', methods=['GET','POST'])
+def add_friend():
+    if request.method == 'POST':
+        form = request.form
+        fname = form['fname']
+        lname = form['lname']
+        # email = form['email']
+        friend_dict = {"name": fname + " " + lname}
+        friend_list.append(friend_dict)
+        return redirect(url_for('mike'))
+    return redirect(url_for('index'))
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
