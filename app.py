84bf29400a1f7b5b019e348fb9415ed133f70239
Nikole Molina <nikole-molina@uiowa.edu>
2022-04-27 01:41:47 -0500

    changed to "nikole"

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ friend_list = [{"name": "Mike Colbert"} ]
 def index():
     return render_template('index.html', pageTitle='Nikole\'s Friends', friends = friend_list)
 
-@app.route('/mike')
+@app.route('/nikole')
 def mike():
-    return render_template('mike.html', pageTitle='About Mike')
+    return render_template('nikole.html', pageTitle='About Nikole')
 
 @app.route('/add_friend', methods=['GET', 'POST'])
 def add_friend():
